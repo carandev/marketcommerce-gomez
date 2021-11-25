2c43a2e0d29e2ebf8349a531c6d2044057b33c50
Carlos Gomez <carlosandres0741@gmail.com>
2021-11-24 19:03:28 -0500

    Add: Key in map

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -4,7 +4,7 @@ import './ItemList.css'
 const ItemList = ({items}) => {
   return (
     <main>
-      {items.map(item => <Item item={item}/>)}
+      {items.map(item => <Item key={item.id} item={item}/>)}
     </main>
   );
 }
