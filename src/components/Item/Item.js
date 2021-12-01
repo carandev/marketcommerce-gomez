8e6439dc81c60f1c271b3d933ef69ba4e8be3edd
Carlos Gomez <carlosandres0741@gmail.com>
2021-12-01 10:33:49 -0500

    Fix: Design in Items

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Item.css'
+import {Link} from "react-router-dom";
 
 const Item = ({item}) => {
   return (
@@ -7,7 +8,7 @@ const Item = ({item}) => {
       <h2>{item.name}</h2>
       <img src={item.img} alt={'test'} />
       <p>$ {item.price}</p>
-      <a href="/item" className="button">Ver producto</a>
+      <Link to={`/item/${item.id}`} className="button">Ver producto</Link>
       <p>Stock disponible: {item.stock}</p>
     </div>
   );
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,10 +1,14 @@
 import Item from '../Item/Item'
 import './ItemList.css'
+import {Link} from "react-router-dom";
 
 const ItemList = ({items}) => {
   return (
     <main>
-      {items.map(item => <Item key={item.id} item={item}/>)}
+      {items.map(item => (
+         <Item key={item.id} item={item}/>
+        )
+      )}
     </main>
   );
 }
