aacfe6ef63d98ca71beaa943dcec7669ed0ef1dc
Carlos Gomez <carlosandres0741@gmail.com>
2022-01-05 20:48:16 -0500

    Add: menu button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CartWidget from "../CartWidget/CartWidget";
 import {Link} from "react-router-dom";
 import ItemCategory from "../ItemCategory/ItemCategory";
 import HeaderStyle from "./HeaderStyle.module.css";
-import {FaShoppingBag} from "react-icons/all";
+import {FaShoppingBag, HiMenu} from "react-icons/all";
 
 const Header = () => {
+
+  const [showMenu, setShowMenu] = useState(false)
+
+  useEffect(() => {
+    if (window.innerWidth > 800) {
+      setShowMenu(true)
+    }
+  }, [])
+
+  const handleMenu = () => {
+    setShowMenu(lastValue => !lastValue)
+  }
+
   return (
     <header className={HeaderStyle.header}>
       <Link to={'/'} className={HeaderStyle.linkLogo}>
-        <FaShoppingBag color="#fff" size={50}/>
+        <FaShoppingBag color="#fff" size={30}/>
         <span>Market Commerce</span>
       </Link>
-      <ItemCategory/>
-      <nav>
-        <CartWidget/>
-      </nav>
+      {
+        showMenu ? null :
+        <HiMenu color="#fff" size={25} onClick={handleMenu}/>
+      }
+      {
+        showMenu &&
+        <nav className={HeaderStyle.navBar}>
+          <ItemCategory/>
+          <CartWidget/>
+        </nav>
+        
+      }
     </header>
   );
 };
